6f89e584388ba5ecab9703514c6e79713061851d
Dhandapani, Baskaran <Baskaran.Dhandapani@ellucian.com>
2020-05-24 13:11:21 +0000

    Added additonal validation

--- FirstConsoleProject/Program.cs
+++ FirstConsoleProject/Program.cs
@@ -12,6 +12,7 @@ namespace FirstConsoleProject
         {
             Console.WriteLine("Welcome to First Console Project");
             Console.Read();
+            Console.ReadLine();
         }
     }
 }
