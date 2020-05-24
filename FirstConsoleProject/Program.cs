f2b14ca1a5b2cde066f51fa834f095744d4c3eb9
Dhandapani, Baskaran <Baskaran.Dhandapani@ellucian.com>
2020-05-24 12:31:00 +0000

    Add project files.

---
+++ FirstConsoleProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to First Console Project");
+            Console.Read();
+        }
+    }
+}
